675e09db430408ff7b418cc03a4ff65e7d402b39
Andrew Meyer <andrewmeyer@granular.ag>
2022-02-15 16:14:44 -0600

    Next version updates
    - build to ./dist instead of root
    - use yarn instead of npm
    - update several dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,101 +1,114 @@
 module.exports = function (grunt) {
-	const sass = require('dart-sass');
+  const sass = require("sass");
 
-	grunt.initConfig({
-		nunjucks: {
-			options: {
-				data: grunt.file.readJSON('data.json'),
-				paths: ['src/templates', 'src/templates/layouts']
-			},
-			render: {
-				files: [{
-					expand: true,
-					cwd: 'src/templates/pages',
-					src: [
-						'**/*.njk',
-						'!**/_*.njk'
-					],
-					dest: './',
-					ext: '.html'
-				}]
-			}
-		},
-		sass: {
-			dist: {
-				options: {
-					implementation: sass,
-					style: 'expanded',
-					sourcemap: 'none'
-				},
-				files: {
-					'css/style.css': 'src/scss/style.scss'
-				}
-			}
-		},
-		postcss: {
-			options: {
-				processors: [
-					require('autoprefixer')(),
-					require('postcss-combine-duplicated-selectors')(),
-					require('cssnano')(),
-				]
-			},
-			dist: {
-				src: 'css/*.css'
-			}
-		},
-		prettify: {
-			options: {
-				indent: 4
-			},
-			all: {
-				expand: true,
-				cwd: './',
-				ext: '.html',
-				src: ['*.html'],
-				dest: './'
-			}
-		},
-		watch: {
-			dev: {
-				files: [
-					'src/scss/**/*.scss',
-					'src/templates/**/*.html',
-					'src/templates/**/*.njk',
-					'data.json'
-				],
-				tasks: ['sass', 'postcss', 'nunjucks', 'prettify']
-			},
-		},
-		browserSync: {
-			dev: {
-				bsFiles: {
-					src: [
-						'./**/*.html',
-						'./css/**/*.css'
-					]
-				},
-				options: {
-					watchTask: true,
-					// proxy: 'bonvoyage.loc'
-					server: {
-						baseDir: "./",
-						directory: true
-					}
-				},
-			}
-		}
-	});
+  grunt.initConfig({
+    nunjucks: {
+      options: {
+        data: grunt.file.readJSON("src/data.json"),
+        paths: ["src/templates", "src/templates/layouts"],
+      },
+      render: {
+        files: [
+          {
+            expand: true,
+            cwd: "src/templates/pages",
+            src: ["**/*.njk", "!**/_*.njk"],
+            dest: "dist",
+            ext: ".html",
+          },
+        ],
+      },
+    },
+    sass: {
+      dist: {
+        options: {
+          implementation: sass,
+          style: "expanded",
+          sourcemap: "none",
+        },
+        files: {
+          "dist/css/style.css": "src/scss/style.scss",
+        },
+      },
+    },
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")(),
+          require("cssnano")(),
+        ],
+      },
+      dist: {
+        src: "dist/css/**/*.css",
+      },
+    },
+    copy: {
+      static: {
+        files: [
+          // copy files from ./src/static to /dist, excluds files starting
+					// with an underscore.
+          {
+            expand: true,
+            cwd: "src/static",
+            src: ["**", "!**/_*/**", "!**/_*"],
+            dest: "dist/",
+          },
+        ],
+      },
+    },
+    prettify: {
+      options: {
+        indent: 2,
+      },
+      all: {
+        expand: true,
+        cwd: "./dist",
+        ext: ".html",
+        src: ["*.html"],
+        dest: "./dist",
+      },
+    },
+    watch: {
+      src: {
+        files: [
+          "src/scss/**/*.scss",
+          "src/templates/**/*.html",
+          "src/templates/**/*.njk",
+          "src/data.json",
+        ],
+        tasks: ["sass", "postcss", "nunjucks", "prettify"],
+      },
+      static: {
+        files: ["src/static/**/*"],
+        tasks: ["copy:static"],
+      },
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ["dist/**/*.html", "dist/css/**/*.css"],
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./dist/",
+          },
+        },
+      },
+    },
+  });
 
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-postcss');
-	grunt.loadNpmTasks('grunt-nunjucks-2-html');
-	grunt.loadNpmTasks('grunt-prettify');
-	grunt.loadNpmTasks('grunt-browser-sync');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("@lodder/grunt-postcss");
+  grunt.loadNpmTasks("grunt-nunjucks-2-html");
+  grunt.loadNpmTasks("grunt-prettify");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
-	grunt.registerTask('css', ['sass', 'postcss']);
-	grunt.registerTask('html', ['nunjucks', 'prettify']);
-	grunt.registerTask('build', ['css', 'html']);
-	grunt.registerTask('default', ['browserSync', 'watch']);
-}
+  grunt.registerTask("css", ["sass", "postcss"]);
+  grunt.registerTask("html", ["nunjucks", "prettify"]);
+  grunt.registerTask("static", ["copy:static"]);
+  grunt.registerTask("build", ["css", "html", "static"]);
+  grunt.registerTask("default", ["browserSync", "watch"]);
+};
---
+++ src/static/_readme.md
@@ -0,0 +1,4 @@
+# What is this?
+
+Files here get copied over to `dist` as-is. If you have need to put additional
+files into the build, this is how you do it.
