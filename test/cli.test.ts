9de935e539c755f78bae7307abdd58d4177f072a
Andrew Meyer <andrew@rewdy.com>
2026-01-26 09:30:45 -0600

    Adds basic ci/cd

---
+++ test/cli.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "bun:test";
+import chalk from "chalk";
+
+describe("Test the cli", () => {
+  it("should pass", () => {
+    // Tests coming soon...
+    console.log(chalk.bgMagenta(" Tests for the CLI will be added soon. "));
+
+    expect(true).toBe(true);
+  });
+});
